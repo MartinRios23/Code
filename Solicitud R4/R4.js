25f149431037f0805685091dfa6987af9666074b
MartinRios23 <martinrioscjs@gmail.com>
2021-05-21 19:11:17 -0300

    Prueba Técnica Finalizada

--- Solicitud R4/R4.js
+++ Solicitud R4/R4.js
@@ -1,10 +1,10 @@
-/*Declaro a la funcion como asíncrona. Ya que debo esperar la respuesta de la API antes de ejecutar el resto del código*/ 
+/*Declaro a la funcion como asíncrona. Ya que debo esperar la respuesta de la API antes de ejecutar el resto del código*/
 
 async function generarTabla() {
-    /**
+  /**
      Ésta constante games espera recibir los datos del fetch
      */
-    const games = await fetch(
+  const games = await fetch(
     "https://api.rawg.io/api/games?key=9e9ca1c80d974269a87013f79911dcee"
   )
     /**
@@ -18,28 +18,28 @@ async function generarTabla() {
       return res.results;
     });
 
-    /*
+  /*
     Traigo la tabla del HTML por el id para luego completarla con los datos recibidos de la API
     */
   var tabla = document.getElementById("Tabla");
   games.forEach((elemento) => {
     var fila = document.createElement("tr");
-    var cell1 = document.createElement("td")
-    var cell2 = document.createElement("td")
-    var cell3 = document.createElement("td")
-    var cell4 = document.createElement("td")
-    cell1.appendChild(document.createTextNode(elemento.name))
-    cell2.appendChild(document.createTextNode(elemento.released))
-    cell3.appendChild(document.createTextNode(elemento.rating))
-    cell4.appendChild(document.createTextNode("Loren impsum"))
-    fila.appendChild(cell1)
-    fila.appendChild(cell2)
-    fila.appendChild(cell3)
-    fila.appendChild(cell4)
-    tabla.appendChild(fila)
-  })
+    var cell1 = document.createElement("td");
+    var cell2 = document.createElement("td");
+    var cell3 = document.createElement("td");
+    var cell4 = document.createElement("td");
+    cell1.appendChild(document.createTextNode(elemento.name));
+    cell2.appendChild(document.createTextNode(elemento.released));
+    cell3.appendChild(document.createTextNode(elemento.rating));
+    cell4.appendChild(document.createTextNode("Loren impsum"));
+    fila.appendChild(cell1);
+    fila.appendChild(cell2);
+    fila.appendChild(cell3);
+    fila.appendChild(cell4);
+    tabla.appendChild(fila);
+  });
 
-    /**
+  /**
     Con esta propiedad, desactivamos el botón 
     para no poder volver a crear una duplicada
     */
